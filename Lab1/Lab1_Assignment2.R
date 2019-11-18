12079306ae6c6b60ba6461941ba2060ec843d936
Christian von Koch <christian.von.koch@gmail.com>
2019-11-19 00:19:07 +0100

    Finished assignment 2

--- Lab1/Lab1_Assignment2.R
+++ Lab1/Lab1_Assignment2.R
@@ -2,13 +2,13 @@
 Dataframe=read.csv2("machines_csv.csv")
 
 #Compute a function for calculating the maximum likelihood of a function
-maxlikelihood=function(theta, x){
+likelihood=function(theta, x){
   n = length(x[,1])
   return(n*log(theta)-theta*sum(x))
 }
 
 #Plot curve for different theta values
-theta_curve = curve(-maxlikelihood(x, Dataframe), from=min(Dataframe), to=max(Dataframe))
+theta_curve = curve(-likelihood(x, Dataframe), from=min(Dataframe), to=max(Dataframe))
 
 #Find maximum likelihood value of theta
 theta_max = function(x){
@@ -25,5 +25,35 @@ y = matrix(Dataframe[1:6,1], nrow=length(Dataframe[1:6,1]), ncol=1)
 print(y)
 
 #Plot new curve on top of each other
-curve(-maxlikelihood(x, Dataframe), from=-1, to=20, add=FALSE, col="red", ylim=c(0,100))
-curve(-maxlikelihood(x, y), from=-1, to=20, add=TRUE, col="blue", ylim=c(0,100))
+curve(-likelihood(x, Dataframe), from=0, to=20, add=FALSE, col="red", ylim=c(0,100))
+curve(-likelihood(x, y), from=0, to=20, add=TRUE, col="blue", ylim=c(0,100))
+
+#Compute a function for calculating the likelihood of the bayesian function
+bayesian_likelihood=function(theta, lambda, x){
+  n = length(x[,1])
+  return(n*log(theta)-theta*sum(x)-lambda*theta)
+}
+
+#Find maximum likelihood value of theta
+bayesian_theta_max = function(lambda, x){
+  n=length(x[,1])
+  return(n/(sum(x)+lambda))
+}
+
+#Find maxtheta
+bayesian_max_theta = bayesian_theta_max(10, Dataframe)
+print(bayesian_max_theta)
+
+#Plot new curve on top of each other
+curve(-bayesian_likelihood(x, 10, Dataframe), from=0, to=20, add=FALSE, col="red", ylim=c(0,100))
+curve(-likelihood(x, Dataframe), from=0, to=20, add=TRUE, col="blue", ylim=c(0,100))
+
+#Generate 50 new observation using theta value from step 2
+set.seed(12345)
+newdata = rexp(50, rate = max_theta)
+
+#Plot new data and old data in histogram
+olddata = Dataframe$Length
+hist(newdata)
+hist(olddata)
+
